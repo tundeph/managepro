656cc5019c8d6196317b4375eebd9569a86101fe
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-19 21:09:53 +0100

    Update Signup.js

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,13 @@
+import { useState } from "react"
+
+//styles
+import "./Signup.css"
+
 const Signup = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [displayName, setDisplayName] = useState("")
+
   return <div>Signup </div>
 }
 
