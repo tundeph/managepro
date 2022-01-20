30ac23cca3e4018f9e45881bc8ea4250d5b133ce
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-20 09:34:51 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
 
 //init firestore
 const manageproDb = firebase.firestore()
