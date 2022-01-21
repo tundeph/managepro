afda2e3508ea705d58833090d4f9d7343de84224
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-22 00:25:14 +0100

    Update Create.js

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from "react"
 import Select from "react-select"
+import { useNavigate } from "react-router-dom"
+import { timestamp } from "../../firebase/config"
 import { useCollection } from "../../hooks/useCollection"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useFirestore } from "../../hooks/useFirestore"
 
 //styles
 import "./Create.css"
+import { Navigate } from "react-router-dom"
 
 const categories = [
   { value: "developemnt", label: "Development" },
@@ -21,7 +26,11 @@ const Create = () => {
   const [assignedUsers, setAssignedUsers] = useState([])
 
   const { documents } = useCollection("users")
+  const { user } = useAuthContext()
   const [users, setUsers] = useState([])
+  const [formError, setFormEror] = useState(null)
+  const { addDocument, response } = useFirestore("projects")
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (documents) {
@@ -33,9 +42,41 @@ const Create = () => {
     }
   }, [documents])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(name, details, dueDate, category.value, assignedUsers)
+    setFormEror(null)
+
+    if (!category) {
+      setFormEror("Please select a project category")
+      return
+    }
+
+    if (assignedUsers.length < 1) {
+      setFormEror("Please assign project to at least one user")
+      return
+    }
+
+    const assignedUsersList = assignedUsers.map((user) => ({
+      displayName: user.value.displayName,
+      photoURL: user.value.photoURL,
+      id: user.value.id,
+    }))
+
+    const project = {
+      name,
+      details,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy: { displayName: user.displayName, photoURL: user.photoURL, id: user.uid },
+      assignedUsersList,
+    }
+
+    await addDocument(project)
+    if (!response.error) {
+      navigate("/")
+    }
+    //console.log(project)
   }
 
   return (
@@ -68,7 +109,7 @@ const Create = () => {
           <span>Assigned to:</span>
           <Select options={users} onChange={(option) => setAssignedUsers(option)} isMulti />
         </label>
-
+        {formError && <p className="error">{formError}</p>}
         <button className="btn">Add Project </button>
       </form>
     </div>
