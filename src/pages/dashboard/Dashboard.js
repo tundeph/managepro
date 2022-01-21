ea8897ad1a76b8c636c1261e17f8c7cb6bc64012
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-22 00:24:41 +0100

    Update Dashboard.js

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,20 @@
+import ProjectList from "../../components/ProjectList"
+import { useCollection } from "../../hooks/useCollection"
+
 //styles and images
+import "./Dashboard.css"
 
 //components
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  const { documents, error } = useCollection("projects")
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  )
 }
 
 export default Dashboard
