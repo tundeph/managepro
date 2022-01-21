e8a96ead503be65ec6ccceeab6eda73c8655ca14
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-21 03:29:08 +0100

    Update Login.js

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,47 @@
+import { useState } from "react"
+import { useLogin } from "../../hooks/useLogin"
+
+//styles
+import "./Login.css"
+
 const Login = () => {
-  return <div>Login</div>
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const { login, isPending, error } = useLogin()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    login(email, password)
+    // console.log("all: ", email, password)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2> Log In.</h2>
+      <label>
+        <span>Email: </span>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
+      </label>
+
+      <label>
+        <span>Password: </span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </label>
+
+      {!isPending && <button className="btn">Login </button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Logging in...
+        </button>
+      )}
+      {error && <div className="error">{error} </div>}
+    </form>
+  )
 }
 
 export default Login
