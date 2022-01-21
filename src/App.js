3b24ff884a1338f32a20c9ded249e6c0932238dc
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-21 03:56:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { useAuthContext } from "../src/hooks/useAuthContext"
 
 //styles
 import "./App.css"
@@ -12,24 +13,36 @@ import Signup from "./pages/signup/Signup"
 import Project from "./pages/project/Project"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
+import Onlineusers from "./components/Onlineusers"
 
 function App() {
+  const { user, authIsReady } = useAuthContext()
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className="container">
-          <Navbar />
+      {authIsReady && (
+        <BrowserRouter>
+          {user && <Sidebar />}
+          <div className="container">
+            <Navbar />
 
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/projects/:id" element={<Project />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+            <Routes>
+              <Route path="/" element={user ? <Dashboard /> : <Navigate replace to="/login" />} />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate replace to="/login" />}
+              />
+              <Route path="/login" element={!user ? <Login /> : <Navigate replace to="/" />} />
+              <Route path="/signup" element={!user ? <Signup /> : <Navigate replace to="/" />} />
+              <Route
+                path="/projects/:id"
+                element={user ? <Project /> : <Navigate replace to="/login" />}
+              />
+            </Routes>
+          </div>
+          {user && <Onlineusers />}
+        </BrowserRouter>
+      )}
     </div>
   )
 }
