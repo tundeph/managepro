84c2a38d33c3e4f1854e0c18483bec185d522281
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-21 18:11:49 +0100

    Update Create.js

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,64 @@
+import { useState } from "react"
+import Select from "react-select"
+
+//styles
+import "./Create.css"
+
+const categories = [
+  { value: "developemnt", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+]
+
 const Create = () => {
-  return <div>Create</div>
+  //form field values
+  const [name, setName] = useState("")
+  const [details, setDetails] = useState("")
+  const [dueDate, setDueDate] = useState("")
+  const [category, setCategory] = useState("")
+  const [assignedUsers, setAssignedUsers] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, details, dueDate, category.value)
+  }
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title"> Create a New Project </h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name: </span>
+          <input type="text" onChange={(e) => setName(e.target.value)} value={name} required />
+          <span>Project details: </span>
+          <textarea
+            type="text"
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+            required
+          ></textarea>
+          <span>Set due date: </span>
+          <input
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+            required
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          <Select options={categories} onChange={(option) => setCategory(option)} />
+        </label>
+        <label>
+          <span>Assigned to:</span>
+          {/* Dropdown */}
+        </label>
+
+        <button className="btn">Add Project </button>
+      </form>
+    </div>
+  )
 }
 
 export default Create
