8af9f1ec813847bb3c61a2ee9835ea2bf9c6807f
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-21 03:57:51 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import { NavLink, Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 //styles and images
 import "./Navbar.css"
 import Temple from "../assets/img/temple.svg"
 
 const Navbar = () => {
+  const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
+
   return (
     <div className="navbar">
       <ul>
@@ -14,16 +19,30 @@ const Navbar = () => {
             <span className="logotext">ManagePro</span>
           </Link>
         </li>
-
-        <li>
-          <NavLink to="/login"> Login </NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup"> Signup </NavLink>
-        </li>
-        <li>
-          <button className="btn">Logout</button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <NavLink to="/login"> Login </NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup"> Signup </NavLink>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button onClick={logout} className="btn">
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button disabled className="btn">
+                Logging out...
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </div>
   )
