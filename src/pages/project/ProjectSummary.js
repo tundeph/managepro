92d918cfe17bcf866d8e16b55f350cb8440086c4
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-22 11:28:29 +0100

    Update ProjectSummary.js

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,22 +1,43 @@
 import Avatar from "../../components/Avatar"
+import { useFirestore } from "../../hooks/useFirestore"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useNavigate } from "react-router-dom"
 
 //styles
 import "./Project.css"
 
 const ProjectSummary = ({ project }) => {
+  const { deleteDocument } = useFirestore("projects")
+  const { user } = useAuthContext()
+  const navigate = useNavigate()
+
+  const handleClick = (e) => {
+    deleteDocument(project.id)
+    navigate("/")
+  }
+
   return (
-    <div className="project-summary">
-      <h2 className="page-title"> {project.name} </h2>
-      <p className="due-date">Project due by {project.dueDate.toDate().toDateString()}</p>
-      <p className="details">{project.details}</p>
-      <h4>Project is assigned to: </h4>
-      <div className="assigned-users ">
-        {project.assignedUsersList.map((user) => (
-          <div key={user.id}>
-            <Avatar src={user.photoURL} />
-          </div>
-        ))}
+    <div>
+      <div className="project-summary">
+        <h2 className="page-title"> {project.name} </h2>
+        <p>Created By: {project.createdBy.displayName} </p>
+        <p className="due-date">Project due by {project.dueDate.toDate().toDateString()}</p>
+        <p className="details">{project.details}</p>
+        <h4>Project is assigned to: </h4>
+        <div className="assigned-users ">
+          {project.assignedUsersList.map((user) => (
+            <div key={user.id}>
+              <Avatar src={user.photoURL} />
+            </div>
+          ))}
+        </div>
       </div>
+
+      {user.uid === project.createdBy.id && (
+        <button onClick={handleClick} className="btn">
+          Mark as Complete
+        </button>
+      )}
     </div>
   )
 }
