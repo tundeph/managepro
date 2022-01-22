3be404286073b8a69c0ca8870d30fdec396e6427
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-22 12:23:22 +0100

    Update ProjectComments.js

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,6 +3,7 @@ import Avatar from "../../components/Avatar"
 import { timestamp } from "../../firebase/config"
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { useFirestore } from "../../hooks/useFirestore"
+import formatDistanceToNow from "date-fns/formatDistanceToNow"
 
 const ProjectComments = ({ project }) => {
   const { updateDocument, response } = useFirestore("projects")
@@ -41,7 +42,7 @@ const ProjectComments = ({ project }) => {
                 <p>{comment.displayName} </p>
               </div>
               <div className="comment-date">
-                <p> date here </p>
+                <p> {formatDistanceToNow(comment.createdAt.toDate(), { addSuffix: true })}</p>
               </div>
               <div className="comment-content">{comment.content}</div>
             </li>
