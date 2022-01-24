f6cec86407b3c715cee82365c207967baeac7c6c
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-24 23:22:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,11 +27,8 @@ function App() {
             <Navbar />
 
             <Routes>
-              <Route path="/" element={user ? <Dashboard /> : <Navigate replace to="/login" />} />
-              <Route
-                path="/create"
-                element={user ? <Create /> : <Navigate replace to="/login" />}
-              />
+              <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+              <Route path="/create" element={user ? <Create /> : <Navigate to="/login" />} />
               <Route path="/login" element={!user ? <Login /> : <Navigate replace to="/" />} />
               <Route path="/signup" element={!user ? <Signup /> : <Navigate replace to="/" />} />
               <Route
